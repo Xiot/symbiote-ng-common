7e26991395f8e3836bcfdf119974968983061dd0
Xiot <xiotox@gmail.com>
2014-12-23 23:15:42 -0500

    added automatic tagging to gulp

--- dist/symbiote-ng-common.js
+++ dist/symbiote-ng-common.js
@@ -106,11 +106,7 @@
                     var httpCache = $cacheFactory.get('$http');
                     httpCache.remove(fullUri);
                 }
-                return $http(request);    //console.log('GET ' + fullUri);
-                                          //return $http.get(fullUri, config)
-                                          //    .finally(function () {
-                                          //        console.log('GET ' + fullUri + ' Complete');
-                                          //    });
+                return $http(request);
             }
             function _getAbsoluteUri(uri) {
                 if (!provider.baseUri)
@@ -140,94 +136,7 @@
             }
         }
         HttpClient.$inject = ["$http", "$q", "$cacheFactory", "storageService"];
-    }    //module CoreServices {
-         //	export class HttpClientProvider implements ng.IServiceProvider {
-         //	    private baseUri: string;
-         //	    public setBaseUri(uri: string): void {
-         //	        this.baseUri = uri;
-         //	}
-         //    /* @ngInject */
-         //    public $get($http, storageService, $q, $cacheFactory): any {
-         //        return new HttpClient($http, storageService, $q, $cacheFactory, this.baseUri);
-         //    }
-         //}
-         //	export interface IHttpClient {
-         //	    get(uri: string, config?: any): ng.IPromise<any>;
-         //	    post(uri: string, data: any, config?: any): ng.IPromise<any>;
-         //	    put(uri: string, data: any, config?: any): ng.IPromise<any>;
-         //	    delete(uri: string, config?: any): ng.IPromise<any>;
-         //	    patch(uri: string, data: any, config?: any): ng.IPromise<any>;
-         //	}
-         //	export class HttpClient implements IHttpClient {
-         //	    // TODO: Move the token stuff to the authentication service
-         //	    // TODO: Add interceptor for 401
-         //	    public static AuthenticationTokenName: string = 'synergize-token';
-         //	    private $http: ng.IHttpService;
-         //	    private baseUri: string;
-         //	    private localCache: CoreServices.IStorageService
-         //	    private $cacheFactory: ng.ICacheFactoryService;
-         //	    constructor($http: ng.IHttpService, storageService: CoreServices.IStorageService, $q, $cacheFactory: ng.ICacheFactoryService, uri) {
-         //	        this.$http = $http;
-         //	        this.baseUri = uri;
-         //	        this.localCache = storageService;
-         //	        this.$cacheFactory = $cacheFactory;
-         //	    }
-         //	    public get(uri: string, config?: any): ng.IPromise<any> {
-         //	        config = config || {};
-         //	    this.setAuthorizationHeaders(config);
-         //	    var fullUri = this.getAbsoluteUri(uri);
-         //	    if (config.forceRefresh) {
-         //	        var httpCache = this.$cacheFactory.get('$http');
-         //	        httpCache.remove(fullUri);
-         //	    }
-         //	    console.log('GET ' + fullUri);
-         //	    return this.$http.get(fullUri, config)
-         //            .finally(() => console.log('GET ' + fullUri + ' Complete'));
-         //	}
-         //public post(uri: string, data: any, config?: any): ng.IPromise<any> {
-         //    config = config || {};
-         //this.setAuthorizationHeaders(config);
-         //var fullUri = this.getAbsoluteUri(uri);
-         //return this.$http.post(fullUri, data, config);
-         //}
-         //public put(uri: string, data: any, config?: any): ng.IPromise<any> {
-         //    return null;
-         //}
-         //public patch(uri: string, data: any, config?: any): ng.IPromise<any> {
-         //    return null;
-         //}
-         //public delete(): ng.IPromise<any> {
-         //    return null;
-         //}
-         ////#region Helpers
-         //private getAbsoluteUri(uri: string): string {
-         //    if (!this.baseUri)
-         //        return uri;
-         //    uri = uri || "";
-         //    if (uri.startsWith('/') && this.baseUri.endsWith('/'))
-         //        uri = uri.substring(1);
-         //    return uri.indexOf('://', 0) < 0
-         //        ? this.baseUri + uri
-         //        : uri;
-         //}
-         //private setAuthorizationHeaders(config): void {
-         //    //TODO: Move the authentication token stuff into an interceptor
-         //    if (config.auth) {
-         //				var authKey = '';
-         //for (var scheme in config.auth) {
-         //    authKey += scheme + ' ' + config.auth[scheme] + ' ';
-         //}
-         //angular.extend(config, { headers: { Authorization: authKey } }, angular.copy(config));				
-         //} else {
-         //    var token = this.localCache.get(HttpClient.AuthenticationTokenName);
-         //    if (token) {
-         //        angular.extend(config, { headers: { Authorization: 'SynergizeToken ' + token } }, angular.copy(config));
-         //    }
-         //}
-         //}
-         ////#endregion
-         //}
-         //} 
+    }
 }());
 (function () {
     'use strict';
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var plug = require('gulp-load-plugins')();
 var wiredep = require('wiredep');
 var es = require('event-stream');
+var semver = require('semver');
+var fs = require('fs');
 
 var run = require('run-sequence');
 
@@ -13,39 +15,93 @@ var paths = {
         templates: 'templates/**/*.html'
     },
     output: {
-        dist: 'dist'        
+        dist: 'dist'
+    },
+    pkg: {
+        bower: './bower.json',
+        npm: './package.json'
     }
 }
 
-gulp.task('compile:src:html', function () {
+gulp.task('compile:src:html', function() {
     return gulp.src(paths.src.templates)
-        .pipe(plug.size({ title: 'before' }))
-        .pipe(plug.htmlmin({ removeComments: true, collapseWhitespace: true }))
+        .pipe(plug.size({
+            title: 'before'
+        }))
+        .pipe(plug.htmlmin({
+            removeComments: true,
+            collapseWhitespace: true
+        }))
         .pipe(plug.angularTemplatecache('symbiote-ng-common.templates.js', {
             module: 'symbiote.templates',
             standalone: true,
             root: 'src/'
         }))
 
-        //.pipe(plug.uglify())
-        .pipe(plug.size({ title: 'after' }))
-    .pipe(plug.size({ title: 'gzip', gzip: true }))
+    //.pipe(plug.uglify())
+    .pipe(plug.size({
+            title: 'after'
+        }))
+        .pipe(plug.size({
+            title: 'gzip',
+            gzip: true
+        }))
         .pipe(gulp.dest(paths.output.dist));
 })
 
-gulp.task('compile:src:js', function () {
+gulp.task('compile:src:js', function() {
     return gulp.src(paths.src.js)
         .pipe(plug.sourcemaps.init())
-        
-        .pipe(plug.wrapJs('(function() {\r\n"use strict";\r\n%= body %\r\n})();', { newline: '\r\n' }))
+
+    .pipe(plug.wrapJs('(function() {\r\n"use strict";\r\n%= body %\r\n})();', {
+            newline: '\r\n'
+        }))
         .pipe(plug.ngAnnotate())
-        
-        .pipe(plug.angularFilesort())
+
+    .pipe(plug.angularFilesort())
         .pipe(plug.concat('symbiote-ng-common.js'))
         //.pipe(plug.uglify())
-        .pipe(plug.sourcemaps.write('./'))        
+        .pipe(plug.sourcemaps.write('./'))
         .pipe(gulp.dest(paths.output.dist));
 });
 gulp.task('default', ['compile:src:js', 'compile:src:html']);
 
+gulp.task('release', ['default'], function() {
 
+    var inc = env.inc;
+    var tag = env.tag;
+    var setVersion = env.version;
+    var newVersion = null;
+
+    if (!setVersion) {
+        var bower = readJson(paths.pkg.bower);
+
+        var current = semver(bower.version);
+        var currentTag = current.prerelease && current.prerelease.length > 0 && current.prerelease[0];
+
+        if (!inc && currentTag) {
+            inc = 'prerelease';
+            tag = tag || currentTag;
+
+        } else if (!inc && !currentTag) {
+            inc = "patch";
+        }
+        newVersion = current.inc(inc, tag);
+    } else {
+        newVersion = semver(setVersion);
+    }
+
+    return gulp.src([paths.pkg.bower, paths.pkg.npm])
+        .pipe(plug.bump({
+            version: newVersion.format()
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(plug.git.commit('chore(release) v' + newVersion))
+        .pipe(plug.filter(paths.pkg.bower))
+        .pipe(plug.tagVersion());
+});
+
+
+var readJson = function(path) {
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+};
