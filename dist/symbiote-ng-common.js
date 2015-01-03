ae03cba51a77fa41e9cabfa66e4f306d4b832832
Xiot <xiotox@gmail.com>
2015-01-03 16:50:26 -0500

    dist

--- dist/symbiote-ng-common.js
+++ dist/symbiote-ng-common.js
@@ -149,10 +149,10 @@
         };
         return service;
         function addListener(eventName, listener) {
-            return $rootScope.on(eventName, listener);
+            return $rootScope.$on(eventName, listener);
         }
         function fireEvent(eventName, args) {
-            $rootScope.emit(eventName, args);
+            $rootScope.$emit(eventName, args);
         }
     }
     EventService.$inject = ["$rootScope"];
