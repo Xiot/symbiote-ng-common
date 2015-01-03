38d3fd18d74a1eea2ec7390c564b741089c5be2c
Xiot <xiotox@gmail.com>
2015-01-03 16:45:08 -0500

    eventService now uses correct method names

--- src/services/data/eventService.js
+++ src/services/data/eventService.js
@@ -12,10 +12,10 @@
  	return service;
 
  	function addListener(eventName, listener) { 		
-        return $rootScope.on(eventName, listener);   
+        return $rootScope.$on(eventName, listener);   
  	}
 
  	function fireEvent(eventName, args) {
-        $rootScope.emit(eventName, args); 		
+        $rootScope.$emit(eventName, args); 		
  	}
  }
